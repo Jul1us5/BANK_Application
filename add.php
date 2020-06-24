fd70b202fa3e770dce33765c6c2cb735f0f706c3
Julius <Julius>
2020-06-24 14:16:55 +0300

    BANK APP. Step 2.

--- add.php
+++ add.php
@@ -4,9 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/font-awesome.min.css">
     <link rel="stylesheet" href="./style/reset.css">
     <link rel="stylesheet" href="./style/main.css">
-    <title>BANK Application | Profile</title>
+    <title>BANK Application | ADD</title>
 </head>
 
 <body>
